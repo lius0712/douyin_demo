205dc719d5dc7056dddc2691e8e301b23485ca56
sduby <supersduby@gmail.com>
2022-06-01 00:03:10 +0800

    feat: 一键重建数据库

--- entity/user.go
+++ entity/user.go
@@ -4,6 +4,7 @@ type User struct {
 	ID            int64  `gorm:"column:uid"`
 	Name          string `gorm:"column:username"`
 	Password      string `gorm:"column:password"`
+	Salt          string `gorm:"column:salt"`
 	FollowCount   int64  `gorm:"column:follow_count"`
 	FollowerCount int64  `gorm:"column:follower_count"`
 	IsFollow      bool   `gorm:"column:is_follow"`
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/RaymondCode/simple-demo/repository"
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
 func main() {
 	if err := Init(); err != nil {
 		os.Exit(-1)
 	}
+
+	if len(os.Args) > 1 && os.Args[1] == "reset" {
+		err := repository.ResetDB()
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		log.Println("reset db success")
+		os.Exit(0)
+	}
+
 	r := gin.Default()
 	initRouter(r)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
